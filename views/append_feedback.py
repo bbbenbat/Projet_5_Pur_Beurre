1e9789cb43750c84b5be1c33eb3455da94960912
bbbenbat <bbessayah@gmail.com>
2020-10-18 21:25:53 +0200

    pep8 append_feedback.py

--- views/append_feedback.py
+++ views/append_feedback.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from controllers import orm
+
 orm_imp = orm.Orm()
+
 
 class Append_fb:
     """  """
+
     def show_old_product(self, list_old_product):
         """  """
         for req in list_old_product:
@@ -14,14 +17,9 @@ class Append_fb:
     def show_product(self, list_product):
         """  """
         for req in list_product:
-            print("Product:",req[0],"Store:", req[1])
+            print("Product:", req[0], "Store:", req[1])
 
     def show_error(self, list_error):
         """  """
         for req in list_error:
-            print("Erreur sur:",req[0], req[1])
-
-
-
-
-
+            print("Erreur sur:", req[0], req[1])
