4a0a8e8a9080095aa23280bf442003fcc4004f94
bbbenbat <bbessayah@gmail.com>
2020-10-19 09:58:43 +0200

    pep8 append_feedback.py

---
+++ venv/Lib/site-packages/models/research.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+from peewee import *
+
+from models import orm_data as od, subcategory as sc, product as pr
+
+
+class Research(od.BaseModel):
+    """ Research table
+    Columns : id, id_subcategory, id_product"""
+    date = DateTimeField(null=True)
+    id_subcategory = ForeignKeyField(column_name='id_subcategory', field='id', model=sc.Subcategory, null=True)
+    id_product = ForeignKeyField(column_name='id_product', field='id', model=pr.Product, null=True)
+
+    class Meta:
+        table_name = 'research'
